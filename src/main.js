3885b7c6a032ab5aab18cfd75babb071251f20a2
Ángeles Buzón <abuzon+git@posteo.es>
2026-02-14 17:02:03 +0100

    Sección razas con javascript funcional

--- src/main.js
+++ src/main.js
@@ -5,65 +5,95 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 */
 
-//WIP: Import race pictures
-
 window.onload = function() {
-    const thumbnails = document.getElementById("raceThumbnails");
-    raceThumbnails.addEventListener("click", changeRaceThumbnailAndPicture);
+    const raceThumbnailList = document.getElementById('raceThumbnails');
+
+    raceThumbnailList.addEventListener("click", handleRaceChange);
 }
 
-const changeRaceThumbnailAndPicture = function(leftClick) {
-    changeThumbnail(leftClick);
-    const thumbnailId = getRaceThumbnailId(leftClick);
-    changeRacePicture(leftClick, idMiniatura);
-}
+const handleRaceChange = function(leftClick) {
+    // Avoid selecting things different to img (ul, li, p, etc.)
+    if (leftClick.target.tagName.toLowerCase() == 'img') {
+        changeRaceThumbnail(leftClick);
 
-const changeThumbnail = function(leftClick) {
-    // 1. Deselect current 'activeThumb'
-    let previousThumbnails = document.getElementById("raceThumbnails").getElementsByClassName("activeThumb");
-    console.debug(previousThumbnails);
+        const newRaceThumbnailId = document.getElementById("raceThumbnails").getElementsByClassName("activeRaceThumb")[0].id;
+        console.log(`New race thumb selected: ${newRaceThumbnailId}`);
 
+        changeRaceInfo(leftClick, newRaceThumbnailId);
+    }
     
+}
+
+const changeRaceThumbnail = function(leftClick) {
+    // 1. Deselect current activeRaceThumb
+    const raceThumbnailElements = document.getElementById("raceThumbnails").querySelectorAll('img');
+    console.warn(raceThumbnailElements);
+
+    // for (let i = 0; i < raceThumbnailElements.length; i++) {
+        // raceThumbnailElements[i].classList.toggle("activeRaceThumb");
+    // }
+
+    const previousRaceThumbnails = document.getElementById("raceThumbnails").getElementsByClassName("activeRaceThumb");
+    console.debug(previousRaceThumbnails);
+
     // if to control that this isn't the first selection ever,
     // [0] because there should be only one pic
-    if (previousThumbnails.length != 0) { 
-        previousThumbnails[0].classList.toggle("activeThumb"); 
+    if (previousRaceThumbnails.length != 0) { 
+        previousRaceThumbnails[0].classList.toggle("activeRaceThumb"); 
     }
 
-    // 2. And then change activePicture
-    leftClick.target.classList.toggle("activeThumb");
+    // 2. And then change activeRaceThumb
+    leftClick.target.classList.toggle("activeRaceThumb");
+
 }
 
-const getRaceThumbnailId = function(leftClick) {
-    let thumbnailId = document.getElementById("raceThumbnails").getElementsByClassName("activeThumb")[0].id;
-    return thumbnailId;
-}
+const changeRaceInfo = function(leftClick, raceThumbnailId) {
+    const raceInfoList = document.getElementById("raceInfos").querySelectorAll("article");
+    const currentActiveRaceInfo = document.getElementById("raceInfos").getElementsByClassName("activeRaceInfo");
 
-const changeRacePicture = function(evento, idMiniatura) {
-    // 1. Get element
-    let picture = document.getElementById("racePicture").getElementsByTagName("img");
-
-    // 2. Cambiar atributos (src, alt, title)
-    // elemento.setAttribute(“nombreAtributo”, “nuevoValor”)
-    // [0] porque solo debería ser una en el array/collection picture
-    let ruta = "./public/img/";
-    let descripcion = "";
-
-    // WIP:
-
-    if (idMiniatura == "miniatura-pichu") {
-        ruta += "pichu_grande.png";
-        descripcion = "Pichu";
-    } else if (idMiniatura == "miniatura-pikachu") {
-        ruta += "pikachu_grande.png";
-        descripcion = "Pikachu";
-    } else if (idMiniatura == "miniatura-raichu") {
-        ruta += "raichu_grande.png";
-        descripcion = "Raichu";
+    let newRaceInfoId = "raceInfo";
+    switch (raceThumbnailId) {
+        case `raceThumbHuman`:
+            newRaceInfoId += 'Human';
+            break;
+        case `raceThumbElf`:
+            newRaceInfoId += 'Elf';
+            break;
+        case `raceThumbDrow`:
+            newRaceInfoId += 'Drow';
+            break;
+        case `raceThumbHalfElf`:
+            newRaceInfoId += 'HalfElf';
+            break;
+        case `raceThumbHalfOrc`:
+            newRaceInfoId += 'HalfOrc';
+            break;
+        case `raceThumbHalfling`:
+            newRaceInfoId += 'Halfling';
+            break;
+        case `raceThumbDwarf`:
+            newRaceInfoId += 'Dwarf';
+            break;
+        case `raceThumbGnome`:
+            newRaceInfoId += 'Gnome';
+            break;
+        case `raceThumbTiefling`:
+            newRaceInfoId += 'Tiefling';
+            break;
+        case `raceThumbGithyanki`:
+            newRaceInfoId += 'Githyanki';
+            break;
+        case `raceThumbDragonborn`:
+            newRaceInfoId += 'Dragonborn';
+            break;
+        default:
+            console.error("Something wrong happened when trying to detect the race ID in changeRaceInfo()");
+            break;
     }
 
-    picture[0].setAttribute("src", ruta);
-    picture[0].setAttribute("alt", descripcion);
-    picture[0].setAttribute("title", descripcion);
-
+    const newRaceInfo = document.getElementById(newRaceInfoId);
+    if (newRaceInfo) {
+        currentActiveRaceInfo[0].classList.remove("activeRaceInfo");
+        newRaceInfo.classList.add("activeRaceInfo");
+    }
 }
